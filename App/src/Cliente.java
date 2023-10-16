fe67128b98c6760093d7b1856755ee2effd5c544
Kesia Pereira <kesiapereira2003@gmail.com>
2023-10-16 16:26:44 -0300

    Historico_Transacoes p1

--- App/src/Cliente.java
+++ App/src/Cliente.java
@@ -2,6 +2,7 @@ import java.util.List;
 import java.time.LocalDate;
 
 public class Cliente {
+
     private String nome;
     private final String cpf;
     private LocalDate dataNasc;
@@ -9,10 +10,43 @@ public class Cliente {
     List<Conta> contas;
 
 
-public Cliente(String nome, String cpf, LocalDate dataNasc, String endereco){
-    this.nome = nome;
-    this.cpf = cpf;
-    this.dataNasc = dataNasc;
-    this.endereco = endereco;
-}
+    public Cliente(String nome, String cpf, LocalDate dataNasc, String endereco){
+        this.nome = nome;
+        this.cpf = cpf;
+        this.dataNasc = dataNasc;
+        this.endereco = endereco;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getCpf(){
+        return cpf;
+    }
+
+    public LocalDate GetDataNasc(){
+        return dataNasc;
+    }
+    public void setDataNasc(LocalDate dataNasc){
+        this.dataNasc = dataNasc;
+    }
+
+    public String getEndereco(){
+        return endereco;
+    }
+    public void setEndereco(String endereco){
+        this.endereco = endereco;
+    }
+
+    public String toString(){
+        return "\nNome: " + this.getNome() +
+        "\nCPF: " + this.getCpf() +
+        "\nNascimento: " + this.GetDataNasc() +
+        "\nEndereco: " + this.getEndereco();
+    }
+
 }
--- App/src/Conta.java
+++ App/src/Conta.java
@@ -1,38 +1,52 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Conta {
     private final String numAgencia;
     private final String numConta;
-    private Double saldo = 0.00;
+    private Double saldo;
     private Cliente cliente;
+    private List<Transacao> transacoes;
 
 
-public Conta(String numAgencia, String numConta, Cliente cliente){
-    this.numAgencia = numAgencia;
-    this.numConta = numConta;
-    this.cliente = cliente;
-}
-
-public double getSaldo(){
-    return saldo;
-}
-
-public void deposito(double valor){
-    saldo += valor;
-}
-
-public void sacar(double valor){
-    if (valor <= saldo){
-        saldo -= valor;
-    } else {
-        System.out.println("Saldo insuficiente.");
+    public Conta(String numAgencia, String numConta, Cliente cliente){
+        this.numAgencia = numAgencia;
+        this.numConta = numConta;
+        this.cliente = cliente;
+        this.saldo = 0.00;
+        this.transacoes = new ArrayList<>();
     }
-}
 
-public void transfer(Conta contaFin, double valor){
-    if (valor <= saldo){
-        saldo -= valor;
-        contaFin.deposito(valor);
-    } else {
-        System.out.println("Saldo insuficiente.");
+    public double getSaldo(){
+        return saldo;
     }
-}
+
+    public void deposito(double valor){
+        saldo += valor;
+    }
+
+    public void sacar(double valor){
+        if (valor <= saldo){
+            saldo -= valor;
+        } else {
+            System.out.println("Saldo insuficiente.");
+        }
+    }
+
+    public void transfer(Conta contaFin, double valor){
+        if (valor <= saldo){
+            saldo -= valor;
+            contaFin.deposito(valor);
+        } else {
+            System.out.println("Saldo insuficiente.");
+        }
+    }
+
+    public List<Transacao> getTransacoes(){
+        return transacoes;
+    }
+
+    public void addTransacao(Transacao transacao){
+        transacoes.add(transacao);
+    }
 }
