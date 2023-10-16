e1095b0794d84312d52da85f97b24f40b6914df4
Kesia Pereira <kesiapereira2003@gmail.com>
2023-10-16 19:08:22 -0300

    Transacoes_Classe e Transacoes_Metodos

--- App/src/Transacao.java
+++ App/src/Transacao.java
@@ -1,3 +1,29 @@
+import java.time.LocalDate;
+
 public class Transacao {
-    
+    private String descricao;
+    private Double valor;
+    private LocalDate data;
+
+    public Transacao(String descricao, double valor, LocalDate data){
+        this.descricao = descricao;
+        this.valor = valor;
+        this.data = data;
+    }
+
+    public String getDescricao(){
+        return descricao;
+    }
+    public void setDescricao(String descricao){
+        this.descricao = descricao;
+    }
+
+    public double getValor(){
+        return valor;
+    }
+
+    public LocalDate getData(){
+        return data;
+    }
+
 }
