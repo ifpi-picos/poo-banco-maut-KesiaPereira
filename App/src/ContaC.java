b5b4d198fdd67d71756649c799f286cd13ba444f
Kesia Pereira <kesiapereira2003@gmail.com>
2023-10-24 15:36:28 -0300

    ContaC_Transfer_tax

--- App/src/ContaC.java
+++ App/src/ContaC.java
@@ -1,10 +1,12 @@
 public class ContaC extends Conta{
 
     private double chequeEspecial;
+    private int transferenciasF;
 
     public ContaC(int numAgencia, int numConta, Cliente cliente, double chequeEspecial) {
         super(numAgencia, numConta, cliente);
         this.chequeEspecial = chequeEspecial;
+        this.transferenciasF = 0;
     }
 
     public double getChequeEspecial() {
@@ -13,6 +15,24 @@ public class ContaC extends Conta{
 
     public void setChequeEspecial(double chequeEspecial) {
         this.chequeEspecial = chequeEspecial;
+    }
+
+    public void transfer(Conta contaFin, double valor){
+        if (valor > (super.getSaldo() + chequeEspecial)){
+            System.out.println("Saldo insuficiente.");
+            return;
+        }
+        if (transferenciasF < 2){
+            super.transfer(contaFin, valor);
+        } else {
+            double taxa = valor * 0.10;
+            if (super.getSaldo() >= valor + taxa){
+                super.transfer(contaFin, valor + taxa);
+            } else {
+                System.out.println("Saldo insuficiente.");
+            }
+        }
+        transferenciasF++;
     }
 
 }
