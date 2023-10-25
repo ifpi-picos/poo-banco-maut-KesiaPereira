c6547603be887321117f27cb2a37541fbeb36a46
Kesia Pereira <kesiapereira2003@gmail.com>
2023-10-25 00:17:44 -0300

    Notificacao_Email

--- App/src/Email.java
+++ App/src/Email.java
@@ -1,3 +1,10 @@
 public class Email {
-    
+    public Notificacao notificacoes;
+
+    public void enviaNotif(String operacao, double valor){
+        System.out.println("Enviar notificacao por email");
+        System.out.println("Operacao desejada: " + operacao);
+        System.out.println("Valor: " + valor);
+        System.out.println("Notificacao enviada!");
+    }
 }
